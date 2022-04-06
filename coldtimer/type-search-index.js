09e4a8b7598d4a8becf9fb605fc6e116fd24c52b
joe <joehosten@outlook.com>
2022-04-06 11:36:25 +0100

    Update dirs

---
+++ coldtimer/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"me.hypews.coldtimer.api","l":"API"},{"p":"me.hypews.coldtimer.api","l":"MemberManager"}];updateSearchResults();
---
+++ seailz/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.joehosten.seailzcore"},{"l":"me.joehosten.seailzcore.core.utils"}];updateSearchResults();
