37929acd84c92275e6a567d87a09508f895297ad
joe <joehosten@outlook.com>
2022-04-06 10:58:10 +0100

    U[date seailzcore docs

--- seailz-commission-docs/type-search-index.js
+++ seailz-commission-docs/type-search-index.js
@@ -1,1 +1,5 @@
+/*
+ * Copyright (c) 2022 Joseph Hosten // joee. All rights reserved.
+ */
+
 typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"me.joehosten.seailzcore.core.utils","l":"ConfigUtils"},{"p":"me.joehosten.seailzcore.core.utils","l":"Logger"},{"p":"me.joehosten.seailzcore.core.utils","l":"Logger.LogLevel"},{"p":"me.joehosten.seailzcore","l":"SeailzCorePlugin"}];updateSearchResults();
